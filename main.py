78d7d22c7959369a48a025c7a7f44798cf7955bf
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2022-05-24 02:10:20 -0400

    function now getting the last 10k messages, only needs channelID
    should build in logic to avoid getting to the end of the list

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import os
-
 from dotenv import load_dotenv
+import json
+import requests
+import pandas as pd
+
+
+#  -----------------SET UP----------------------
 load_dotenv()
-PASSWORD = os.getenv('PASSWORD')
+# set up vars
+auth = os.getenv('AUTHORIZATION')
+channelID = 875443001579671555
+# https://discord.com/api/v9/channels/875443001579671555/messages?limit=50
+
+headers = {
+    'authorization': auth
+}
+#  -----------------END-SETUP----------------------
+#  -----------------DEFINE METHODS----------------------
+# goals: get the last thousand messages
+def getMessages(channelID):
+    # request messages
+    df = pd.DataFrame()
+    message_dict = {}
+
+    for x in range(100):
+        if (x == 0):
+            req = requests.get(f'https://discord.com/api/v9/channels/{channelID}/messages?limit=100', headers=headers)
+            message_dict[x] = json.loads(req.text)
+            df = pd.DataFrame(message_dict[x])
+            with open('dataDump.json', 'w') as convert_file:
+                convert_file.write(json.dumps(message_dict))
+        else:
+            last_message_id = df["id"][len(df.index)-1]
+            print(last_message_id)
+            req = requests.get(f'https://discord.com/api/v9/channels/{channelID}/messages?limit=100&before={last_message_id}', headers=headers)
+            message_dict[x] = json.loads(req.text)
+            tmp_df = pd.DataFrame(message_dict[x])
+            df = pd.concat([df, tmp_df], ignore_index=True)
+
+    # df = pd.DataFrame(columns= ['Content', 'Sender', 'TimeStamp'])
 
 
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    # df = pd.concat([df, df2], ignore_index=True)
+    return df
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+#  -----------------END-METHODS----------------------
+
+#  -----------------RUNNING----------------------
+df = getMessages(channelID)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+print('donzo')
 
 
-print(PASSWORD)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#  -----------------END-RUN----------------------
